1ce81b7d527cf391870cedbc4524a9b84bb477b3
Dan Todd <57022952+dannyodd@users.noreply.github.com>
2021-10-08 21:39:17 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# homework-eight
+ Intro to jQuery Assignment
+ Uses javascript to load page content on a single page when nav links are clicked
+ 
+
+ ## URL Link
+ https://in-info-web4.informatics.iupui.edu/~danttodd/homework-eight/
+
+ ## Github Link
+ https://github.com/dannyodd/homework-eight
---
+++ app/app.js
@@ -0,0 +1,371 @@
+var homeContent = `<div id="hero"><h1>Cheffing.com</h1></div>
+<div class="welcome">
+    <h2>Welcome to Cheffing.com!</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem 
+        aut iusto tempora in corporis ut ullam necessitatibus natus, 
+        velit excepturi nulla repellendus libero recusandae a incidunt
+        enim cupiditate, tempore assumenda? Lorem ipsum dolor sit amet 
+        consectetur, adipisicing elit. A totam rem libero facilis 
+        ratione sit, quos quae distinctio, ea expedita placeat numquam! 
+        Quaerat eos magni facere nobis ab cumque accusantium.
+    </p>
+</div>
+<div class="featured-recipes">
+    <h2>Featured Recipes</h2>
+    <div class="feature-container">
+        <div class="feature-item">
+            <h3>Mushroom Thyme Risotto</h3>
+            <a href="#">
+                <img src="images/recipes/recipes-mushthymeriso.jpeg" alt="Mushroom Thyme Risotto">
+            </a>
+        </div>
+        <div class="feature-item">
+            <h3>Greek Gyro Pita</h3>
+            <a href="#">
+                <img src="images/recipes/recipes-gyro.jpeg" alt="Greek Gyro Pita">
+            </a>
+        </div>
+        <div class="feature-item">
+            <h3>Shrimp Scampi Linguine</h3>
+            <a href="#">
+                <img src="images/recipes/recipes-scampi.jpeg" alt="Shrimp Scampi Linguine">
+            </a>
+        </div>
+    </div>
+</div>
+<div class="blog-feed">
+    <h2>Blog Posts</h2>
+    <div class="blog-container">
+        <div class="blog-item">
+            <img src="images/blog/blog-planning.jpeg" alt="Meal Planning Blog Post">
+            <a href="#">How to Meal Plan Like a Pro</a>
+        </div>
+        <div class="blog-item">
+            <img src="images/blog/blog-prepday.jpeg" alt="Prep Day Blog Post">
+            <a href="#">Efficient kitchen prep for the whole week</a>
+        </div>
+        <div class="blog-item">
+            <img src="images/blog/blog-storage.jpeg" alt="Food Storage Blog Post">
+            <a href="#">Our Guide to Safe & Effective Food Storage</a>
+        </div>
+        <div class="blog-item">
+                <img src="images/blog/blog-expiration.jpeg" alt="Expiration Blog Post">
+            <a href="#">Spoilage and You: How to tell if food is actually expired</a>
+        </div>
+    </div>
+</div>
+<div class="kitchen-tools">
+    <h2>Kitchen Tools</h2>
+    <div class="tools-container">
+        <div class="tool-item">
+            <h3>Calculators and Converters</h3>
+            <a href="#">
+                <img src="images/tools/tools-calculator.jpeg" alt="Kitchen Calculators">
+            </a>
+        </div>
+        <div class="tool-item">
+            <h3>Advanced Meal Planner</h3>
+            <a href="#">
+                <img src="images/tools/tools-mealplanner.jpeg" alt="Meal Planner">
+            </a>
+        </div>
+        <div class="tool-item">
+            <h3>Grocery List</h3>
+            <a href="#">
+                <img src="images/tools/tools-grocerylist.jpeg" alt="Grocery List">
+            </a>
+        </div>
+    </div>
+    <div class="get-app">
+        <h2>Download the Cheffing App!</h2>
+        <p>
+            Enjoy access to thousands of recipes and our powerful 
+            toolkit in one convenient, easy to use package. Take 
+            charge of your kitchen, and start Cheffing today!
+        </p>
+    </div>
+</div>`;
+var recipesContent = `        <div id="hero"><h1>Cheffing Cookbook</h1></div>
+<div class="welcome">
+    <h2>Welcome to the Cookbook</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem 
+        aut iusto tempora in corporis ut ullam necessitatibus natus, 
+        velit excepturi nulla repellendus libero recusandae a incidunt
+        enim cupiditate, tempore assumenda? Lorem ipsum dolor sit amet 
+        consectetur, adipisicing elit. A totam rem libero facilis 
+        ratione sit, quos quae distinctio, ea expedita placeat numquam! 
+        Quaerat eos magni facere nobis ab cumque accusantium.
+    </p>
+</div>
+<div class="featured-recipes">
+    <h2>Featured Recipes</h2>
+    <div class="feature-container">
+        <div class="feature-item-expl">
+            <div class="feature-item-expl-img">
+                <h3>Mushroom Thyme Risotto</h3>
+                <img src="images/recipes/recipes-mushthymeriso.jpeg" alt="">
+            </div>
+            <div class="feature-item-expl-text">
+                <div class="feature-item-expl-text-inner">
+                    <h4>Ingredients</h4>
+                    <ul>
+                        <li>Mushrooms</li>
+                        <li>Thyme</li>
+                        <li>Arborio Rice</li>
+                        <li>Vegetable Stock</li>
+                        <li>Butter</li>
+                    </ul>
+                </div>
+                <div class="feature-item-expl-text-inner">
+                    <h4>Directions</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium, facere laboriosam! Vitae, commodi explicabo iste minus eos eaque distinctio quod eum facere animi, beatae assumenda soluta ea unde corporis ducimus!</p>
+                </div>
+            </div>
+        </div>
+        <div class="feature-item-expl">
+            <div class="feature-item-expl-img">
+                <h3>Greek Gyro Pita</h3>
+                <img src="images/recipes/recipes-gyro.jpeg" alt="">
+            </div>
+            <div class="feature-item-expl-text">
+                <div class="feature-item-expl-text-inner">
+                    <h4>Ingredients</h4>
+                    <ul>
+                        <li>Ground Lamb</li>
+                        <li>Ground Beef</li>
+                        <li>Spices</li>
+                        <li>Cucumber</li>
+                        <li>Greek Yogurt</li>
+                    </ul>
+                </div>
+                <div class="feature-item-expl-text-inner">
+                    <h4>Directions</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium, facere laboriosam! Vitae, commodi explicabo iste minus eos eaque distinctio quod eum facere animi, beatae assumenda soluta ea unde corporis ducimus!</p>
+                </div>
+            </div>
+        </div>
+        <div class="feature-item-expl">
+            <div class="feature-item-expl-img">
+                <h3>Shrimp Scampi Linguine</h3>
+                <img src="images/recipes/recipes-scampi.jpeg" alt="">
+            </div>
+            <div class="feature-item-expl-text">
+                <div class="feature-item-expl-text-inner">
+                    <h4>Ingredients</h4>
+                    <ul>
+                        <li>Shrimp</li>
+                        <li>Lemon</li>
+                        <li>Butter</li>
+                        <li>White Wine</li>
+                        <li>Garlic</li>
+                    </ul>
+                </div>
+                <div class="feature-item-expl-text-inner">
+                    <h4>Directions</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium, facere laboriosam! Vitae, commodi explicabo iste minus eos eaque distinctio quod eum facere animi, beatae assumenda soluta ea unde corporis ducimus!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="featured-recipes">
+    <h2>All Recipes</h2>
+    <div class="feature-container">
+        <div class="feature-item">
+            <h3>Recipe Name</h3>
+            <a href="#">
+                <img src="images/recipes/recipes-scampi.jpeg" alt="">
+            </a>
+        </div>
+        <div class="feature-item">
+            <h3>Recipe Name</h3>
+            <a href="#">
+                <img src="images/recipes/recipes-scampi.jpeg" alt="">
+            </a>
+        </div>
+        <div class="feature-item">
+            <h3>Recipe Name</h3>
+            <a href="#">
+                <img src="images/recipes/recipes-scampi.jpeg" alt="">
+            </a>
+        </div>
+        <div class="feature-item">
+            <h3>Recipe Name</h3>
+            <a href="#">
+                <img src="images/recipes/recipes-scampi.jpeg" alt="">
+            </a>
+        </div>
+        <div class="feature-item">
+            <h3>Recipe Name</h3>
+            <a href="#">
+                <img src="images/recipes/recipes-scampi.jpeg" alt="">
+            </a>
+        </div>
+        <div class="feature-item">
+            <h3>Recipe Name</h3>
+            <a href="#">
+                <img src="images/recipes/recipes-scampi.jpeg" alt="">
+            </a>
+        </div>
+    </div>
+</div>`;
+var toolsContent = `<div id="hero"><h1>Cheffing Tools</h1></div>
+<div class="welcome">
+    <h2>Welcome to the Toolkit</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem 
+        aut iusto tempora in corporis ut ullam necessitatibus natus, 
+        velit excepturi nulla repellendus libero recusandae a incidunt
+        enim cupiditate, tempore assumenda? Lorem ipsum dolor sit amet 
+        consectetur, adipisicing elit. A totam rem libero facilis 
+        ratione sit, quos quae distinctio, ea expedita placeat numquam! 
+        Quaerat eos magni facere nobis ab cumque accusantium.
+    </p>
+</div>
+<div class="blog-main">
+    <h2>Cheffing Tools</h2>
+    <div class="blog-item">
+        <img src="images/tools/tools-calculator.jpeg" alt="">
+        <div class="blog-text">
+            <h3>Cost Calculators, Converters & More</h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Odit, saepe possimus explicabo ipsa enim cum, illum nesciunt 
+                eum aperiam architecto eveniet ut rerum quos! Velit doloremque 
+                accusantium labore cum laudantium?
+            </p>
+            <div class="blog-button">
+                <h3>Go To Tool</h3>
+            </div>
+        </div>
+    </div>
+    <div class="blog-item">
+        <img src="images/tools/tools-mealplanner.jpeg" alt="">
+        <div class="blog-text">
+            <h3>Advanced Meal Planner</h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Odit, saepe possimus explicabo ipsa enim cum, illum nesciunt 
+                eum aperiam architecto eveniet ut rerum quos! Velit doloremque 
+                accusantium labore cum laudantium?
+            </p>
+            <div class="blog-button">
+                <h3>Go To Tool</h3>
+            </div>
+        </div>
+    </div>
+    <div class="blog-item">
+        <img src="images/tools/tools-grocerylist.jpeg" alt="">
+        <div class="blog-text">
+            <h3>Integrated Grocery List</h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Odit, saepe possimus explicabo ipsa enim cum, illum nesciunt 
+                eum aperiam architecto eveniet ut rerum quos! Velit doloremque 
+                accusantium labore cum laudantium?
+            </p>
+            <div class="blog-button">
+                <h3>Go To Tool</h3>
+            </div>
+        </div>
+    </div>
+</div>`;
+var blogContent = `<div id="hero"><h1>Cheffing Blog</h1></div>
+<div class="welcome">
+    <h2>Welcome to the Cheffing Blog</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem 
+        aut iusto tempora in corporis ut ullam necessitatibus natus, 
+        velit excepturi nulla repellendus libero recusandae a incidunt
+        enim cupiditate, tempore assumenda? Lorem ipsum dolor sit amet 
+        consectetur, adipisicing elit. A totam rem libero facilis 
+        ratione sit, quos quae distinctio, ea expedita placeat numquam! 
+        Quaerat eos magni facere nobis ab cumque accusantium.
+    </p>
+</div>
+<div class="blog-main">
+    <h2>Blog Posts</h2>
+    <div class="blog-item">
+        <img src="images/blog/blog-planning.jpeg" alt="">
+        <div class="blog-text">
+            <h3>How to Meal Plan Like a Pro</h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Odit, saepe possimus explicabo ipsa enim cum, illum nesciunt 
+                eum aperiam architecto eveniet ut rerum quos! Velit doloremque 
+                accusantium labore cum laudantium?
+            </p>
+            <div class="blog-button">
+                <h3>Read More</h3>
+            </div>
+        </div>
+    </div>
+    <div class="blog-item">
+        <img src="images/blog/blog-prepday.jpeg" alt="">
+        <div class="blog-text">
+            <h3>Efficient Kitchen Prep for the Whole Week</h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Odit, saepe possimus explicabo ipsa enim cum, illum nesciunt 
+                eum aperiam architecto eveniet ut rerum quos! Velit doloremque 
+                accusantium labore cum laudantium?
+            </p>
+            <div class="blog-button">
+                <h3>Read More</h3>
+            </div>
+        </div>
+    </div>
+    <div class="blog-item">
+        <img src="images/blog/blog-storage.jpeg" alt="">
+        <div class="blog-text">
+            <h3>Our Guide to Safe & Effective Food Storage</h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Odit, saepe possimus explicabo ipsa enim cum, illum nesciunt 
+                eum aperiam architecto eveniet ut rerum quos! Velit doloremque 
+                accusantium labore cum laudantium?
+            </p>
+            <div class="blog-button">
+                <h3>Read More</h3>
+            </div>
+        </div>
+    </div>
+    <div class="blog-item">
+        <img src="images/blog/blog-expiration.jpeg" alt="">
+        <div class="blog-text">
+            <h3>Spoilage and You: How to Tell if Food is Actually Expired</h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Odit, saepe possimus explicabo ipsa enim cum, illum nesciunt 
+                eum aperiam architecto eveniet ut rerum quos! Velit doloremque 
+                accusantium labore cum laudantium?
+            </p>
+            <div class="blog-button">
+                <h3>Read More</h3>
+            </div>
+        </div>
+    </div>
+</div>`;
+
+
+function initListeners() {
+    $("nav a").click(function(e){
+        // This stores the id from the anchor tag
+        let aID = e.currentTarget.id;
+        let contentID = aID + "Content";  
+        let heroImg = aID + "-hero.jpg";
+        
+        
+        $("#app").html(eval(contentID));
+        $("#hero").css('background-image', 
+        'linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(../images/hero/' + heroImg + ')');
+    });
+}
+
+
+$(document).ready(function() {
+    initListeners();
+    $("#app").html(homeContent);
+});
